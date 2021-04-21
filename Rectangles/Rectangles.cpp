563a8fd65a3d2ae8eaa6e44259332303b5420b19
Sirotenko Olga <78899446+syrotenkoo@users.noreply.github.com>
2021-04-22 02:39:45 +0300

    Update Rectangles.cpp

--- Rectangles/Rectangles.cpp
+++ Rectangles/Rectangles.cpp
@@ -2,15 +2,23 @@
 #include <math.h>
 using namespace std;
 
-
 float f(float x)
 {
 	return exp(-x * -x);
 }
 
-
-float rect_integ(float a, float b, int n)
+void rectangles()
 {
+	float a, b;
+	int n;
+
+	cout << "Lower limit of integration a = ";
+	cin >> a;
+	cout << "Upper limit of integration b = ";
+	cin >> b;
+	cout << "Number of subintervals n = ";
+	cin >> n;
+
 	float width = (b - a) / n;
 	float rect_integ = 0;
 	for (int step = 0; step < n; step++)
@@ -18,21 +26,11 @@ float rect_integ(float a, float b, int n)
 		float x = a + step * width;
 		rect_integ += width * f(x + 0.5 * width);
 	}
-	return rect_integ;
-}
 
+	cout << "Integral is equal to: " << rect_integ << endl;
+}
 
 int main()
 {
-	float a, b;
-	int n;
-	cout << "Method of Rectangles" << endl;
-	cout << "Lower limit of integration a = ";
-	cin >> a;
-	cout << "Upper limit of integration b = ";
-	cin >> b;
-	cout << "Number of subintervals n = ";
-	cin >> n;
-	cout << "Integral is equal to: " << rect_integ(a, b, n) << endl;
-    return 0;
+	rectangles();
 }
