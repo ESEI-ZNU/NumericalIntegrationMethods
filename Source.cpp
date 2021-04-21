ed7840c3d4b0f59b1a0ec659a521fce084c41b99
Maqasta <63746799+Maqasta@users.noreply.github.com>
2021-04-21 11:51:35 +0300

    Create Source.cpp
    update new function (trapezium)

---
+++ Source.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+#define f(x) exp(-x*-x)
+using namespace std;
+
+void trapezium()
+{
+	float lower, upper, integration = 0.0, stepSize, k, subInterval;
+
+	//input
+	cout << "Enter lower limit of integration: ";
+	cin >> lower;
+	cout << "Enter upper limit of integration: ";
+	cin >> upper;
+	cout << "Enter number of sub intervals: ";
+	cin >> subInterval;
+
+	//Calculation
+	stepSize = (upper - lower) / subInterval; //Finding step size 
+
+	//Finding Integration Value
+	integration = f(lower) + f(upper);
+	for (int i = 1; i <= subInterval - 1; i++)
+	{
+		k = lower + i * stepSize;
+		integration = integration + 2 * (f(k));
+	}
+	integration = integration * stepSize / 2;
+
+	cout << endl << "Integration = " << integration;
+}
+
+int main()
+{
+	trapezium();
+}
+
