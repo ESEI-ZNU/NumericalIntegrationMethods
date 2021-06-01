1a400b51da928d2c5e1d669e976ff64ab2c44f81
Klymov Danylo <klymovd@gmail.com>
2021-06-01 22:31:42 +0300

    Finally update

--- Simpson.cpp
+++ Simpson.cpp
@@ -1,65 +1,24 @@
-﻿#include<iostream>
+#include<iostream>
 #include<cmath>
+
+#define f(x) exp(-x*-x)
+
 using namespace std;
 
-double f(double x)
+/// <summary>
+///    <para> Simpson's calculating function </para>
+/// </summary>
+void mainSimpson(double lowborder, double upborder, double subinterval)
 {
-	return exp(-x * -x); 
+    const double width = (upborder - lowborder) / subinterval;
+
+    double simpson_integral = 0;
+    for (int step = 0; step < subinterval; step++) {
+        const double x1 = lowborder + step * width;
+        const double x2 = lowborder + (step + 1) * width;
+
+        simpson_integral += (x2 - x1) / 6.0 * (f(x1) + 4.0 * f(0.5 * (x1 + x2)) + f(x2));
+    }
+
+    cout << "\nIntegration = " << simpson_integral << endl;
 }
-
-double simpsonsRule(double a, double b, int n) {
-	double h = 0; 
-	double ifx = 0;
-
-	h = abs(b - a) / n;
-
-	ifx = ifx + f(a) + f(b); 
-
-	for (double i = a + h; i < b; i++) {
-		ifx = ifx + (4 * f(i));
-	}
-
-	for (double j = a + (2 * h); j < b; j++) {
-		ifx = ifx + (2 * f(j));
-	}
-
-	ifx = ifx * h / 3; //розрахункова формула Сімпсона при інтегруванні на відрізку [a, b]
-	return ifx;
-}
-
-double mainSimpson() {
-	int choose;
-
-	cout << "Task: to implement methods of numerical integration on a user-specified interval." << endl << "Simpson's method." << endl; 
-	
-	do {
-		double a, b; 
-		int n = 2; 
-		cout << "\nEnter a value for the left border: ";
-		cin >> a;
-		cout << endl << "Enter a value for the right border: ";
-		cin >> b;
-		while (a > b)
-		{
-			cout << "The left border must be less than the right." << endl << "Enter new value: ";
-			cin >> b;
-		} 
-		if (n % 2 == 0) 
-			cout << endl << "The definite integral value: " << simpsonsRule(a, b, n) << endl;
-		else
-			cout << "Error! " << "The value must be paired." << endl;
-		cout << "\nDo you want to start over?" << endl; 
-		cout << "1) - Yes, 2) - No" << endl;
-		cout << "Your choice: ";
-		cin >> choose;
-	} while (choose == 1);
-
-	return choose;
-}
-
-int main()
-{
-	mainSimpson();
-}
-
-
