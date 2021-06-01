8fd21bcfd1be57014763a6aef8695b37aafc5e4d
Klymov Danylo <klymovd@gmail.com>
2021-06-01 22:30:01 +0300

    Finally update

--- trapezium.cpp
+++ trapezium.cpp
@@ -1,37 +1,31 @@
-#include<iostream>
-#include<math.h>
+#include <iostream>
+#include <math.h>
+
 #define f(x) exp(-x*-x)
+
 using namespace std;
 
-void trapezium()
+/// <summary>
+///    <para> Method of Trapezium </para>
+/// </summary>
+///<return>
+///    <para> Turning code </para>
+/// </return>
+void trapezium(double lowborder, double upborder, double subinterval)
 {
-	float lower, upper, integration = 0.0, stepSize, k, subInterval;
-
-	//input
-	cout << "Enter lower limit of integration: ";
-	cin >> lower;
-	cout << "Enter upper limit of integration: ";
-	cin >> upper;
-	cout << "Enter number of sub intervals: ";
-	cin >> subInterval;
+	float integration = 0.0, stepSize, k;
 
 	//Calculation
-	stepSize = (upper - lower) / subInterval; //Finding step size 
+	stepSize = (upborder - lowborder) / subinterval; //Finding step size 
 
 	//Finding Integration Value
-	integration = f(lower) + f(upper);
-	for (int i = 1; i <= subInterval - 1; i++)
+	integration = f(lowborder) + f(upborder);
+	for (int i = 1; i <= subinterval - 1; i++)
 	{
-		k = lower + i * stepSize;
+		k = lowborder + i * stepSize;
 		integration = integration + 2 * (f(k));
 	}
 	integration = integration * stepSize / 2;
 
-	cout << endl << "Integration = " << integration;
+	cout << endl << "Integration = " << integration << endl;
 }
-
-int main()
-{
-	trapezium();
-}
-
